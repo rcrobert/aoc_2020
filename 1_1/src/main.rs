bbd48cb3ceae7f7ce132a35302600fbcd22410c1
Reese Robertson <rrobertson@purestorage.com>
2020-11-30 22:28:32 -0800

    Day 1.1 Solution

--- 1_1/src/main.rs
+++ 1_1/src/main.rs
@@ -1,3 +1,53 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
+
+type InputType = i32;
+const SHITHOLE_YEAR: InputType = 2020;
+
 fn main() {
-    println!("Hello, world!");
+    let mut file = File::open("input").expect("could not open input file");
+
+    let inputs = collect_inputs(&mut file);
+    do_magic(&inputs);
+}
+
+fn collect_inputs(file: &mut File) -> Vec<InputType> {
+    let mut reader = BufReader::new(file);
+
+    let mut inputs = Vec::<InputType>::new();
+
+    loop {
+        let mut buf = String::new();
+        match reader.read_line(&mut buf) {
+            Ok(x) if x != 0 => {
+                if let Ok(val) = buf.trim_end().parse::<InputType>() {
+                    inputs.push(val);
+                } else {
+                    panic!("failed to parse line {}", buf);
+                }
+            }
+
+            // Finished or failed reading
+            Ok(_) => break,
+            Err(_) => panic!("failed to read line"),
+        }
+    }
+
+    return inputs;
+}
+
+fn do_magic(values: &Vec<InputType>) {
+    for i in 0..values.len() {
+        for j in i + 1..values.len() {
+            let l = values[i];
+            let r = values[j];
+            if l + r == SHITHOLE_YEAR {
+                // Found it
+                println!("{} * {} = {}", l, r, l * r);
+                return;
+            }
+        }
+    }
+    panic!("could not find a {} summing pair", SHITHOLE_YEAR);
 }
