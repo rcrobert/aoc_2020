0faf6e2d334ac86dc9f3d03a0ebcd4ed71894fbd
Reese Robertson <reese.c.robertson@gmail.com>
2020-12-09 11:36:22 -0800

    Day 6.2 Solution

--- src/bin/day_6.rs
+++ src/bin/day_6.rs
@@ -3,8 +3,9 @@ extern crate my;
 use my::input::InputReader;
 use std::collections::BTreeSet;
 
+type Answers = BTreeSet<char>;
 struct GroupAnswers {
-    answers: BTreeSet<char>,
+    answers: Vec<Answers>,
 }
 
 fn main() {
@@ -15,25 +16,45 @@ fn main() {
     for line in reader.lines() {
         // Empty line is the group delimiter, start tracking a new one
         if line.len() == 0 {
-            accum += current_group.answers.len() as u64;
+            accum += tally_group(&current_group);
             current_group = GroupAnswers::new();
             continue;
         }
+        let mut passenger_answers = BTreeSet::<char>::new();
         for c in line.chars() {
-            current_group.answers.insert(c);
+            passenger_answers.insert(c);
         }
+        current_group.answers.push(passenger_answers);
     }
 
     // Finally tally the last group
-    accum += current_group.answers.len() as u64;
+    accum += tally_group(&current_group);
 
     println!("Magic sum: {}", accum);
+}
+
+/// Returns the number of questions every group member responded 'True' to.
+fn tally_group(g: &GroupAnswers) -> u64 {
+    if g.answers.len() == 0 {
+        return 0;
+    }
+
+    let mut answers = g.answers.iter();
+    let initial: Answers = answers.next().unwrap().clone();
+    let common_answers = answers.fold(initial, |accum, member_answers| {
+        accum
+            .intersection(member_answers)
+            .cloned()
+            .collect()
+    });
+
+    return common_answers.len() as u64;
 }
 
 impl GroupAnswers {
     fn new() -> Self {
         Self {
-            answers: BTreeSet::new(),
+            answers: Vec::new(),
         }
     }
 }
